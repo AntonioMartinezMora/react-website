5377fbfc9493de34fe258dbd0f660146a1ac9241
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2021-02-24 12:31:20 -0500

    Created NavBar. Need to be fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import NavBar from './components/NavBar';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const App = () => {
     return (
-        <div>
-            <h1>Try this</h1>
-        </div>
+        <>
+        <Router>
+            <NavBar />
+            <Switch>
+                <Route path="/" exact />
+            </Switch>
+        </Router>
+        </>
     )
 }
 
